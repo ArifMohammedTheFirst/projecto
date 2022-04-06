dc4c20f1df782f48b1026e5ad5795684aef87240
whh <wenhaoh5@nycstudents.net>
2022-04-06 13:20:26 -0400

    Initial commit

---
+++ src/Accesswordle.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Accesswordle {
+
+    public static void importWords(File )
+}
---
+++ src/wordle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class wordle {
+
+    private String Keyword;
+
+
+    public wordle(){
+        setGame();
+    }
+
+    public void setGame(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello welcome to Wordle");
+
+    }
+
+}
